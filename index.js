65c91a11b4dd2a8d5b074dadd71f774505c148fa
LombreBlanche34 <54815949+LombreBlanche34@users.noreply.github.com>
2019-11-27 19:25:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,5 +54,24 @@ client.on('message', async message => {
             .setFooter("Bot fait par LB", "https://cdn.discordapp.com/avatars/495683052152946708/a_9a41054830ead278cee8eeec3cde1be0.gif")
         message.channel.send(embedHelp)
     }
+
+    if (args[0].toLocaleLowerCase() === prefix + 'kick'){
+        membre = message.mentions.members.first()
+        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(message.author.username + ", tu n'as pas la permission de kick des membres `KICK MEMBERS` !")
+        if (!membre) return message.channel.send(message.author.username + ", tu dois mentioné quelqu'un !")
+        if (membre.hasPermission("ADMINISTRATOR")) return message.channel.send(message.author.username + ", tu ne peut pas kick cette personne !")
+        message.guild.member(membre).kick(membre)
+        message.channel.send("✅ " + membre + " a ete kick")
+    }
+
+    if (args[0].toLocaleLowerCase() === prefix + 'ban'){
+        membre = message.mentions.members.first()
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(message.author.username + ", tu n'as pas la permission de BAN des membres `BAN MEMBERS` !")
+        if (!membre) return message.channel.send(message.author.username + ", tu dois mentioné quelqu'un !")
+        if (membre.hasPermission("ADMINISTRATOR")) return message.channel.send(message.author.username + ", tu ne peut pas ban cette personne !")
+        message.guild.member(membre).ban(membre)
+        message.channel.send("✅ " + membre + " a ete ban")
+    }
+
 });
 client.login(config.Token);
