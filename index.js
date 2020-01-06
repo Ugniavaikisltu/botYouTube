3ad2b91c7eb914d2ae5e3095d3e04e6bfbbcc27b
LombreBlanche34 <54815949+LombreBlanche34@users.noreply.github.com>
2020-01-06 18:31:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var config = require('./config.json')
 const client = new Discord.Client();
 const fs = require('fs')
 let xp = JSON.parse(fs.readFileSync('./xp.json', 'utf8'))
+let bl = JSON.parse(fs.readFileSync('./blacklist.json', 'utf8'))
 var prefix = config.Prefix
 
 client.on('ready', () => {
@@ -11,6 +12,20 @@ client.on('ready', () => {
 });
 
 client.on("guildMemberAdd", member => {
+    if (!bl[member.id]) {
+        bl[member.id] = {
+            bl: "false"
+        }
+        fs.writeFile("./blacklsit.json", JSON.stringify(bl, null, 4), err => {
+            if (err) throw err;
+        })
+    }
+    if (bl[member.id].bl === "true") {
+        member.send("tu a été blacklist, tu ne peut donc pas rejoindre se server")
+        member.guild.channels.get("634477535001968650").send(member + " est dans la blacklist, ila donc été banni definitivement.")
+        member.ban()
+        return
+    } return
     var embed = new Discord.RichEmbed()
         .setColor("#2eff00")
         .setTitle("Bienvenue !")
@@ -96,7 +111,7 @@ client.on('message', async message => {
 
     //ecoute 
     if (message.content === prefix + "ecoute") {
-        client.user.setActivity("%help !", {type: "LISTENING"});
+        client.user.setActivity("%help !", { type: "LISTENING" });
         message.channel.send("mon status a bien été changer en `ecoute %help !`")
     }
 
@@ -116,6 +131,7 @@ client.on('message', async message => {
     // del
     if (args[0].toLocaleLowerCase() === prefix + 'del') {
         let count = args[1]
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("tu n'as pas la permission")
         if (!count) return message.channel.send("veuillez indiquer un nombre de message a supprimer")
         if (isNaN(count)) message.channel.send("veuillez indiquer un nombre valide")
         if (count < 1 || count > 100) return message.channel.send("veuillez indiquer un nombre entre 1 et 100")
@@ -128,10 +144,26 @@ client.on('message', async message => {
         let textP = args.slice(2).join(" ");
         if (!textP) return message.channel.send(" entre du texte")
         var embedP = new Discord.RichEmbed()
-        .setColor(colorP)
-        .setDescription(textP)
+            .setColor(colorP)
+            .setDescription(textP)
         message.channel.send(embedP)
     }
+
+    //blacklist commande
+    if (args[0].toLocaleLowerCase() === prefix + 'blacklist') {
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("tu n'as pas la permission `ADMINISTRATOR`")
+        let blU = message.mentions.users.first()
+        if (!blU) return message.channel.send("Tu dois mentionner quelqu'un !")
+        bl[blU.id] = {
+            bl: "true"
+        }
+        fs.writeFile("./blacklist.json", JSON.stringify(bl, null, 4), err => {
+            if (err) throw err;
+        })
+        message.channel.send(blU.username + " a bien été blacklist :skull_crossbones: !")
+    }
+
+
 
     //add rank
     if (message.content === message.content) {
@@ -160,7 +192,7 @@ client.on('message', async message => {
         return
     }
 
-   
+    // ne pas mettre de commande apres le bloc // add rank
 
 });
 client.login(config.Token);
