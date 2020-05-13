f2303ff9a56b561a0d03aa7f882a1c7c47af5bc5
LombreBlanche34 <54815949+LombreBlanche34@users.noreply.github.com>
2020-05-13 21:20:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,47 @@ let xp = JSON.parse(fs.readFileSync('./xp.json', 'utf8'))
 let bl = JSON.parse(fs.readFileSync('./blacklist.json', 'utf8'))
 var prefix = config.Prefix
 
+const size = config.colors;
+const rainbow = new Array(size);
+
+for (var i = 0; i < size; i++) {
+    var red = sin_to_hex(i, 0 * Math.PI * 2 / 3);
+    var blue = sin_to_hex(i, 1 * Math.PI * 2 / 3);
+    var green = sin_to_hex(i, 2 * Math.PI * 2 / 3);
+
+    rainbow[i] = '#' + red + green + blue;
+}
+
+function sin_to_hex(i, phase) {
+    var sin = Math.sin(Math.PI / size * 2 * i + phase);
+    var int = Math.floor(sin * 127) + 128;
+    var hex = int.toString(16);
+
+    return hex.length === 1 ? '0' + hex : hex;
+}
+
+let place = 0;
+const servers = config.servers;
+
+function changeColor() {
+    for (let index = 0; index < servers.length; ++index) {
+        client.guilds.get(servers[index]).roles.find(x => x.id === config.roleName).setColor(rainbow[place])
+            .catch(console.error);
+
+        if (config.logging) {
+            console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+        }
+        if (place == (size - 1)) {
+            place = 0;
+        } else {
+            place++;
+        }
+    }
+}
+
 client.on('ready', () => {
     console.log('pret !')
+    setInterval(changeColor, config.speed)
     client.user.setActivity("%help", {
         type: "WATCHING"
     });
